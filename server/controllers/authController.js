d26ae921037558039da41fa90898529fa9390363
jayyrt <jayy.rt19@gmail.com>
2021-09-23 20:28:53 -0600

    debugging

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,8 +7,8 @@ module.exports = {
         const result = await db.get_user([username]);
         const existingUser = result[0];
         if (existingUser) {
-            return res.status(409).send('Username taken')
-        };
+            return res.status(409).send('Username taken');
+        }
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(password, salt);
         const registeredUser = await db.register_user([isAdmin, username, hash]);
@@ -18,7 +18,16 @@ module.exports = {
     },
     login: async (req, res) => {
         const { username, password } = req.body;
-        const db = req.app.get('db');
-        
-    }
+        const foundUser = await req.app.get('db').get_user([username]);
+        const user = foundUser[0];
+        if(!user) {
+            return res.status(401).send('User not found. Please register as a new user before logging in.')
+        }
+        const isAuthenticated = bcrypt.compareSync(password, user.hash);
+        if(!isAuthenticated) {
+            return res.status(403).send('Incorrect password');
+        }
+        req.session.user = { isAdmin: user.is_admin, id: user.id, username: user.username };
+        return res.send(req.session.user);
+    },
 };
